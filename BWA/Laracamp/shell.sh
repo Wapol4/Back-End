9d456fbcd92695604a0d7bbc88b05e00ce9a52a5
AiristEsp <eairist@gmail.com>
2022-12-21 06:33:15 +0700

    Migration Camp Table

--- BWA/Laracamp/shell.sh
+++ BWA/Laracamp/shell.sh
@@ -1,2 +1,6 @@
 1. Create Laravel project
     - composer create-project laravel/laravel="8.6.2" Laracamp
+
+2. Migration database:
+    - php artisan make:migration create_table_camps --table=camps
+    - php artisan make:model Camp
