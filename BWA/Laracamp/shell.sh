aad3d3ab0cc84df1288bf15841551387b3e07354
AiristEsp <eairist@gmail.com>
2022-12-21 06:39:18 +0700

    Migration Camp Benefit Table

--- BWA/Laracamp/shell.sh
+++ BWA/Laracamp/shell.sh
@@ -2,5 +2,10 @@
     - composer create-project laravel/laravel="8.6.2" Laracamp
 
 2. Migration database:
+    Camp Table
     - php artisan make:migration create_table_camps --table=camps
     - php artisan make:model Camp
+
+    Camp Benefits
+    // Create model and migration
+    - php artisan make:model CampBenefit -m
