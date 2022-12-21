00edf4a2f4a9d443db15e8f1b6cf925bf1254c35
AiristEsp <eairist@gmail.com>
2022-12-21 08:22:14 +0700

    Migration Checkout Table

--- BWA/Laracamp/app/Models/Checkout.php
+++ BWA/Laracamp/app/Models/Checkout.php
@@ -6,12 +6,16 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Camp extends Model
+class Checkout extends Model
 {
     use HasFactory, SoftDeletes;
 
     protected $fillable = [
-        'title', 
-        'price',
+        'user_id', 
+        'camp_id',
+        'card_number',
+        'expired',
+        'cvc',
+        'is_paid',
     ];
 }
