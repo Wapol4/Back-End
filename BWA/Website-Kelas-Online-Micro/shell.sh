87ed683498bcc13959e106ca16c4c4be715ee70e
AiristEsp <eairist@gmail.com>
2022-12-22 08:10:22 +0700

    Set-Up Api Gateway

---
+++ BWA/Website-Kelas-Online-Micro/shell.sh
@@ -0,0 +1,7 @@
+1. Install Express Generator
+    - sudo npm install -g express-generator
+2. Create API Gateway
+    - express --no-view api-gateway
+    - cd api-gateway
+    - npm install
+    - npm start
